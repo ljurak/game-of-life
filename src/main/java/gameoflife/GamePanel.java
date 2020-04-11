b880cf363e7fb5683fbb20d574c68ff82ec85a35
ljurak <lucas.jurak@gmail.com>
2020-04-11 23:41:06 +0200

    Add panel to display the game board

---
+++ src/main/java/gameoflife/GamePanel.java
@@ -0,0 +1,47 @@
+package gameoflife;
+
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+public class GamePanel extends JPanel {
+
+    private static final int DEFAULT_WIDTH = 400;
+    private static final int DEFAULT_HEIGHT = 400;
+
+    private final GameEngine gameEngine;
+
+    public GamePanel(GameEngine gameEngine) {
+        this.gameEngine = gameEngine;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+
+        int size = gameEngine.getSize();
+        boolean[][] board = gameEngine.getBoard();
+
+        int cellWidth = getWidth() / size;
+        int cellHeight = getHeight() / size;
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                g2.drawRect(j * cellWidth, i * cellHeight, cellWidth, cellHeight);
+
+                g2.setPaint(Color.BLACK);
+                if (board[i][j]) {
+                    g2.fillRect(j * cellWidth, i * cellHeight, cellWidth, cellHeight);
+                }
+            }
+        }
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+}
