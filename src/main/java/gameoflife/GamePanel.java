3800de22d3f46e782097b11f7daea019c98db240
ljurak <lucas.jurak@gmail.com>
2020-04-13 23:26:03 +0200

    Add methods for setting cell colour and game engine

--- src/main/java/gameoflife/GamePanel.java
+++ src/main/java/gameoflife/GamePanel.java
@@ -8,13 +8,23 @@ import java.awt.Graphics2D;
 
 public class GamePanel extends JPanel {
 
-    private static final int DEFAULT_WIDTH = 400;
-    private static final int DEFAULT_HEIGHT = 400;
+    private static final int DEFAULT_WIDTH = 600;
+    private static final int DEFAULT_HEIGHT = 600;
 
-    private final GameEngine gameEngine;
+    private GameEngine gameEngine;
+
+    private Color cellColor = Color.BLACK;
 
     public GamePanel(GameEngine gameEngine) {
         this.gameEngine = gameEngine;
+    }
+
+    public void setGameEngine(GameEngine gameEngine) {
+        this.gameEngine = gameEngine;
+    }
+
+    public void setCellColor(Color cellColor) {
+        this.cellColor = cellColor;
     }
 
     @Override
@@ -28,7 +38,7 @@ public class GamePanel extends JPanel {
         int cellWidth = getWidth() / size;
         int cellHeight = getHeight() / size;
 
-        g2.setPaint(Color.BLACK);
+        g2.setPaint(cellColor);
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 g2.drawRect(j * cellWidth, i * cellHeight, cellWidth, cellHeight);
